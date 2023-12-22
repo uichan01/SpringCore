8f5713e8727b417d855ed808efdd683e3c9f8af6
uichan01 <uichan02@naver.com>
2023-12-22 23:34:34 +0900

    섹션3 AppConfig 리팩터링
    스프링으로 전환하기 전

--- src/main/java/example/core/AppConfig.java
+++ src/main/java/example/core/AppConfig.java
@@ -1,20 +1,27 @@
 package example.core;
 
+import example.core.discount.DiscountPolicy;
 import example.core.discount.FixDiscountPolicy;
-import example.core.member.Member;
-import example.core.member.MemberService;
-import example.core.member.MemberServiceImpl;
-import example.core.member.MemoryMemberRepository;
+import example.core.discount.RateDiscountPolicy;
+import example.core.member.*;
 import example.core.order.OrderService;
 import example.core.order.OrderServiceImpl;
 
 public class AppConfig {
 
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(MemberRepository());
+    }
+
+    private MemberRepository MemberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(MemberRepository(), DiscountPolicy());
+    }
+
+    private DiscountPolicy DiscountPolicy() {
+        return new RateDiscountPolicy();
     }
 }
--- src/test/java/example/core/order/OrderServiceTest.java
+++ src/test/java/example/core/order/OrderServiceTest.java
@@ -1,16 +1,24 @@
 package example.core.order;
 
+import example.core.AppConfig;
 import example.core.member.Grade;
 import example.core.member.Member;
 import example.core.member.MemberService;
 import example.core.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.internal.matchers.Or;
 
 public class OrderServiceTest {
-    MemberService memberService = new MemberServiceImpl();
-    OrderService orderService = new OrderServiceImpl();
+    MemberService memberService;
+    OrderService orderService;
+    @BeforeEach
+    public void beforeEach(){
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderService();
+    }
 
     @Test
     void createOrder(){
