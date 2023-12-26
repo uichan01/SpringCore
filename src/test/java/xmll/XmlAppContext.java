278644139968594ffeec9815d8ce928598e1a689
uichan01 <uichan02@naver.com>
2023-12-26 21:50:32 +0900

    섹션4

---
+++ src/test/java/xmll/XmlAppContext.java
@@ -0,0 +1,18 @@
+package xmll;
+
+import example.core.member.MemberService;
+import example.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class XmlAppContext {
+
+    @Test
+    void xmlAppContext() {
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
