64f4e35a4e001e450083f0d7cd852edfd310d609
uichan01 <uichan02@naver.com>
2024-01-08 23:55:57 +0900

    섹션7 다양한 의존관계~

--- src/main/java/example/core/order/OrderServiceImpl.java
+++ src/main/java/example/core/order/OrderServiceImpl.java
@@ -14,7 +14,20 @@ public class OrderServiceImpl implements OrderService{
     private DiscountPolicy discountPolicy;
 
     @Autowired
+    public void setMemberRepository(MemberRepository memberRepository) {
+        //System.out.println("memberRepository = " + memberRepository);
+        this.memberRepository = memberRepository;
+    }
+    @Autowired
+    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+        //System.out.println("discountPolicy = " + discountPolicy);
+        this.discountPolicy = discountPolicy;
+    }
+
+    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        //System.out.println("memberRepository = " + memberRepository);
+        //System.out.println("discountPolicy = " + discountPolicy);
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
